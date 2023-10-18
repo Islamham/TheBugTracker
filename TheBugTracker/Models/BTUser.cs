db2b93dbb0a4a822b1734252ac8991a25ca14d97
Islamham <hamza98@student.ubc.ca>
2023-10-17 18:50:10 -0700

    BTUser updated (BTUser and Projects Join Table)

--- TheBugTracker/Models/BTUser.cs
+++ TheBugTracker/Models/BTUser.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -19,5 +23,28 @@ namespace TheBugTracker.Models
         [NotMapped] //Do not create a field in DB user table for "Fullname"
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } } //No need to set
+
+        public int? CompanyId  { get; set; } //? allows nullable (User will temporarily not have a company)
+
+
+        //Avatar
+        [NotMapped] //will not be copied to database
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string AvatarContentType { get; set; }
+
+
+        //Navigation Properties
+        public virtual Company Company { get; set; } //obtained by CompanyId
+
+        public virtual ICollection<Project> Projects { get; set; }
+
+
     }
 }
