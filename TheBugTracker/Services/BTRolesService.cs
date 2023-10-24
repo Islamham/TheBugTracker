720a537e5856c2872581c5bbc89b7902364b6032
Islamham <hamza98@student.ubc.ca>
2023-10-24 16:44:46 -0700

    BTRolesService class created

---
+++ TheBugTracker/Services/BTRolesService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTRolesService : IBTRolesService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BTUser> _userManager;
+
+        public BTRolesService(ApplicationDbContext context, 
+                                RoleManager<IdentityRole> roleManager,
+                                UserManager<BTUser> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
