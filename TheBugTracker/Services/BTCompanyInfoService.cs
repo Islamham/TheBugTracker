bd26e79d82da1672464ac2d0e35af665a9bef667
Islamham <hamza98@student.ubc.ca>
2023-10-25 18:02:30 -0700

    GetAllMembersAsync method implemented

---
+++ TheBugTracker/Services/BTCompanyInfoService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BTCompanyInfoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            List<BTUser> result = new();
+
+            result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+
+            return result;
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
