e9d1af06a9cde6317c6bacc51a1455663cc4fd53
Islamham <hamza98@student.ubc.ca>
2023-11-29 15:34:20 -0800

    IBTFileService interface and implementation class created. Some methods implemented.

---
+++ TheBugTracker/Services/BTFileService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTFileService : IBTFileService
+    {
+
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            try
+            {
+                string imageBase64Data = Convert.ToBase64String(fileData);
+                return string.Format($"data:{extension};base64,{imageBase64Data}");
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            try
+            {
+                MemoryStream memoryStream = new();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteFile = memoryStream.ToArray();
+                memoryStream.Close();
+                memoryStream.Dispose();
+                
+                return byteFile;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public string FormatFileSize(long bytes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetFileIcon(string file)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
