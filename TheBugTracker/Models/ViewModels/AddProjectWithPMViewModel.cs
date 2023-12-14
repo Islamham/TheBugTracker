5196a8fc200e78a2220c24cb9478938c9ffd0612
Islamham <hamza98@student.ubc.ca>
2023-12-14 14:58:25 -0800

    Projects Create View implemented

--- TheBugTracker/Models/ViewModels/AddProjectWithPMViewModel.cs
+++ TheBugTracker/Models/ViewModels/AddProjectWithPMViewModel.cs
@@ -12,7 +12,7 @@ namespace TheBugTracker.Models.ViewModels
 
         public SelectList PriorityList { get; set; }
 
-        public int ProjectPriority { get; set; }'
+        public int ProjectPriority { get; set; }
         
     }
 }
